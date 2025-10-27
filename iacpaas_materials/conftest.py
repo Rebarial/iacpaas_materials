b7d053a8feb73a073e8060ba29498520ed5b3f8c
Rebarial <reberalko@gmail.com>
2025-10-27 18:04:41 +1000

    init cookiecutter

---
+++ iacpaas_materials/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from iacpaas_materials.users.models import User
+from iacpaas_materials.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
