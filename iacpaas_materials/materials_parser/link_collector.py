717925401f911be0f2b041595efb7dc3b1d661d6
Warpollio <92286992+Warpollio@users.noreply.github.com>
2026-01-18 22:53:23 +1000

    Materials parser: product_links - product_data separation

--- iacpaas_materials/materials_parser/link_collector.py
+++ iacpaas_materials/materials_parser/link_collector.py
@@ -1,8 +1,10 @@
 from typing import Dict, Any, List
 from .storage import load_sources, save_sources, update_or_add_source
 from .crawler import crawl_site
+import hashlib
 
-
+def url_to_id(url: str) -> str:
+    return hashlib.sha1(url.encode('utf-8')).hexdigest()[:12]
 
 def collect_product_links(source: Dict[str, Any], max_pages: int = 777777, max_depth: int = 10, max_workers: int = 10) -> Dict[str, Any]:
 
@@ -16,8 +18,18 @@ def collect_product_links(source: Dict[str, Any], max_pages: int = 777777, max_d
 
     product_links = [
         {
+            "id":  url_to_id(url),
             "link": url,
             "type":data["Type"],
+ #           "text": data["Text"]
+        }
+        for url, data in results.items()
+        if data['IsProduct'] is True
+    ]
+
+    product_data = [
+        {
+            "id":  url_to_id(url),
             "text": data["Text"]
         }
         for url, data in results.items()
@@ -27,11 +39,11 @@ def collect_product_links(source: Dict[str, Any], max_pages: int = 777777, max_d
     return {
         "name": source['name'],
         "product_links": product_links
-    }
+    }, product_data
 
-def process_and_save_source(source: Dict[str, Any], output_file: str = "product_links.json", max_pages: int = 777777, max_depth: int = 10, num_threads: int = 10) -> None:
+def process_and_save_source(source: Dict[str, Any], output_links_file: str = "product_links.json", output_data_file: str = "product_data.json", max_pages: int = 777777, max_depth: int = 10, num_threads: int = 10) -> None:
     """Обёртка: собрать + сохранить."""
-    new_source = collect_product_links(source, max_pages, max_depth, num_threads)
-    data = load_sources(output_file)
+    new_source, product_data = collect_product_links(source, max_pages, max_depth, num_threads)
+    data = load_sources(output_links_file)
     update_or_add_source(data, new_source)
-    save_sources(data, output_file)
+    save_sources(data, product_data, output_links_file, output_data_file)
